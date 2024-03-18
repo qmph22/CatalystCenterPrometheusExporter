6bbabf39e340421b51f98d0f4fafdbfe9118db2a
qmph22 <66791972+qmph22@users.noreply.github.com>
2024-03-18 13:34:53 -0400

    Removed configurations for DockProm and added Prometheus Docker

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 rm -rf ./dockprom
-mkdir dockprom
+#mkdir dockprom
+mkdir prometheus
 npm install
-cd dockprom
-git clone https://github.com/stefanprodan/dockprom.git .
+#cd dockprom
+cd prometheus
+#git clone https://github.com/stefanprodan/dockprom.git .
 mv ./prometheus/prometheus.yml ./prometheus/prometheus.yml.bak
 cp ../config/prometheus.yml ./prometheus/
 echo $1
@@ -13,6 +15,10 @@ cp ../DNAC_USER_CONFIG_TPL.js ../DNAC_USER_CONFIG.js
 sed -i '' "s/IP_ADDRESS/$2/g" ../DNAC_USER_CONFIG.js
 sed -i '' "s/USER_NAME/$3/g" ../DNAC_USER_CONFIG.js
 sed -i '' "s/USER_PASSWORD/$4/g" ../DNAC_USER_CONFIG.js
-docker-compose up -d
+#docker-compose up -d
+docker run \
+    -p 9090:9090 \
+    -v ./prometheus/prometheus.yml:/etc/prometheus/prometheus.yml \
+    prom/prometheus
 cd ..
 node ./server.js
